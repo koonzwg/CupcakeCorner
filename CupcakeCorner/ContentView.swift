299349a8090b028f84e90ee963ab747bfbf9b679
koonzwg <koonzwg@gmail.com>
2024-04-16 13:14:38 -0500

    Finished project

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -6,41 +6,59 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
+    @State private var order = Order()
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
         do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
+            engine = try CHHapticEngine()
+            try engine?.start()
         } catch {
-            print("Invalid data")
+            print("There was an error creating the engine: \(error.localizedDescription)")
         }
     }
-    
+
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+
+                    if order.specialRequestEnabled {
+                        
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink("Delivery details") {
+                        AddressView(order: order)
+                    }
+                }
             }
-        }
-        .task {
-            await loadData()
+            .navigationTitle("Cupcake Corner")
         }
     }
 }
-
 #Preview {
     ContentView()
 }
